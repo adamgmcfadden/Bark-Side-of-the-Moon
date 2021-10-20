63a550752dfc923ed225a4459bb6c599c0c135ef
Lakyn <lakynmphotography@hotmail.com>
2021-10-20 01:52:45 -0400

    signup, login, Donation Setup

--- client/src/pages/Dashboard/index.js
+++ client/src/pages/Dashboard/index.js
@@ -1,1 +1,15 @@
-// dashboard code here
+// Dashboard code here
+import React from 'react';
+
+function Dashboard() {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <div class="favourites">
+        <h2>Favourites</h2>
+      </div>
+
+    </div>
+  );
+}
+     export default Dashboard;
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,1 +1,8 @@
 // queries code here
+export const QUERY_CHECKOUT = gql`
+  query getCheckout($animals: [ID]!) {
+    checkout(animals: $animals) {
+      session
+    }
+  }
+`;
