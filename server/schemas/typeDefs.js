d0bee6020b9c8aa870e600ad544a30aad8bf43c9
Adam Girard-McFadden <adam_m_20@hotmail.com>
2021-10-21 13:50:35 -0400

    models and typeDefs complete

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,1 +1,44 @@
-// typeDefs here
+// import gql dependency
+const { gql } = require("apollo-server-express");
+
+// create typeDefs
+const typeDefs = gql`
+  type Query {
+    me: User
+  }
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    savedPet(input: savedPets!): User
+    removePet(petId: String!): User
+  }
+  type User {
+    _id: ID!
+    username: String
+    email: String
+    petCount: Int
+    savedPets: [Pet]
+  }
+  type Pet {
+    petId: String
+    name: String
+    description: String
+    image: String
+    link: String
+  }
+  input savedPet {
+    name: String
+    breed: String
+    description: String
+    petId: String
+    image: String
+    link: String
+  }
+  type Auth {
+    token: ID!
+    user: User
+  }
+`;
+
+// export the typeDefs to index.js
+module.exports = typeDefs;
