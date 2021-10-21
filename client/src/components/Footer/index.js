f960bc10bd5cc4d4ca49b512f68853d845208290
wizeeee <wizeman22@outlook.com>
2021-10-20 20:53:10 -0400

    header/footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,1 +1,9 @@
 // footer code here
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="container">BARK-SIDE-OF-THE-MOON ❤️ &copy;</div>
+    </footer>
+  );
+};
+export default Footer;
--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,1 +1,41 @@
-// header code here
+// import all dependencies - React + {useState} and components/pages
+import React, { useState } from "react";
+import Nav from "../Navigation";
+import AdoptionForm from "../../pages/AdoptionForm";
+
+function Header() {
+  // useState to set current page and handleChange
+  const [currentPage, handlePageChange] = useState("About");
+  // switch statement to render pages based on nav-tab clicked
+  const renderPage = () => {
+    switch (currentPage) {
+      case "About":
+        return <About />;
+      case "Portfolio":
+        return <Portfolio />;
+      case "Contact":
+        return <ContactForm />;
+      case "Resume":
+        return <Resume />;
+      default:
+        return <About />;
+    }
+    return <AdoptionForm />;
+  };
+
+  return (
+    <div>
+      <div>
+        <header>
+          {/* useState for current page and handlepagechange */}
+          <Nav currentPage={currentPage} handlePageChange={handlePageChange} />
+        </header>
+      </div>
+      {/* handle page change */}
+      {renderPage(currentPage)}
+    </div>
+  );
+}
+
+// export header function
+export default Header;
