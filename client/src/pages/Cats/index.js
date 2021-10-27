9144382cf73c58f7a2503c06861a18e252d87a47
jack.loveday95@gmail.com <jack.loveday95@gmail.com>
2021-10-26 21:50:29 -0400

    Trying to fix cards, hard coded card working..

--- client/src/pages/Cats/index.js
+++ client/src/pages/Cats/index.js
@@ -11,74 +11,112 @@ const fillerIMG = require("../../assets/images/paws.jpg");
 
 // Setup Petfinder API Client
 const pfClient = new petfinder.Client({ apiKey: api_key, secret: api_secret });
+const catArray2 = [];
+
+
+// Array which will query and save a cats array of cats
+function catData() {
+  // Use petfinder client to make a query by type
+  pfClient.animal
+    .search({
+      type: pageType,
+    })
+    .then((res) => {
+      // Check if response exists
+      if (!res) return false;
+
+      // Then set the data
+      const searchResults = res.data.animals;
+
+      //console.log(searchResults);
+
+      // Create an Array we will load our cats into
+      const cats = [];
+      for (let i = 0; i < 10; i++) {
+        const catName = searchResults[i].name;
+        console.log(catName);
+
+
+        if (!searchResults[i].primary_photo_cropped) {
+          const currentCat = {
+            age: searchResults[i].age,
+            breed: searchResults[i].breeds.primary,
+            description: searchResults[i].description,
+            //image: JSON.stringify(fillerIMG),
+            link: searchResults[i].url,
+            name: searchResults[i].name
+          };
+          //const stringCat = JSON.stringify(currentCat);
+
+          catArray2.push(currentCat);
+        } else {
+          const currentCat =
+          {
+            age: searchResults[i].age,
+            breed: searchResults[i].breeds.primary,
+            description: searchResults[i].description,  
+            //image: searchResults[i].primary_photo_cropped.small.val(),
+            link: searchResults[i].url,
+            name: catName
+          };
+          //const stringCat = JSON.stringify(currentCat);
+
+          catArray2.push(currentCat);
+        }
+      }
+      //return cats;
+    });
+}
 
 function catCards() {
-  const newCatData = [];
-
-  // Array which will query and save a cats array of cats
-  function catData() {
-    // Use petfinder client to make a query by type
-    pfClient.animal
-      .search({
-        type: pageType,
-      })
-      .then((res) => {
-        // Check if response exists
-        if (!res) return false;
-
-        // Then set the data
-        const searchResults = res.data.animals;
-
-        //console.log(searchResults);
-
-        // Create an Array we will load our cats into
-        const cats = [];
-        for (let i = 0; i < searchResults.length; i++) {
-          if (!searchResults[i].primary_photo_cropped) {
-            let currentCat = {
-              name: searchResults[i].name,
-              description: searchResults[i].description,
-              breed: searchResults[i].breeds.primary,
-              age: searchResults[i].age,
-              link: searchResults[i].url,
-              image: fillerIMG,
-            };
-            newCatData.push(currentCat);
-          } else {
-            let currentCat = {
-              name: searchResults[i].name,
-              description: searchResults[i].description,
-              breed: searchResults[i].breeds.primary,
-              age: searchResults[i].age,
-              link: searchResults[i].url,
-              image: searchResults[i].primary_photo_cropped.small,
-            };
-            newCatData.push(currentCat);
-          }
-        }
-        return cats;
-      });
-  }
   catData();
-  console.log(newCatData);
+
+
+  const holder = [];
+  const dummy =
+  {
+    age: "Adult",
+    breed: "Domestic Short Hair",
+    description: "This is Cinnamon. She is 4 year old gal who was originally adopted as a\nkitten. Unfortunately, the family recently...",
+    image: "https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/53382957/1/?bust=1635295589&width=300",
+    link: "https://www.petfinder.com/cat/cinnamon-53382957/oh/cincinnati/hart-oh445/?referrer_id=1917ceca-bc45-468e-adfb-9373212df0dd",
+    name: "Cinnamon"
+  }
+  const dummy2 =
+  {
+    age: "Adult",
+    breed: "Domestic Short Hair",
+    description: "This is Cinnamon. She is 4 year old gal who was originally adopted as a\nkitten. Unfortunately, the family recently...",
+    image: "https://dl5zpyw5k3jeb.cloudfront.net/photos/pets/53382957/1/?bust=1635295589&width=300",
+    link: "https://www.petfinder.com/cat/cinnamon-53382957/oh/cincinnati/hart-oh445/?referrer_id=1917ceca-bc45-468e-adfb-9373212df0dd",
+    name: "Cinnamon2"
+  }
+
+  // Holder is working array that is hard coded
+  // catArray2 is working array but wont load cards
+
+  holder.push(dummy, dummy2);
+  console.log(catArray2);
+  console.log(holder);
+
   return (
     <div className="catscards row justify-content-center">
       <h1 className="cats">Cats</h1>
-      {newCatData.map((cats) => {
+      {catArray2.map((catArray, i) => {
         return (
-          <div className="cats-cards">
+          <div className="cats-cards" key={i}>
             <img
               className="img-cats"
-              src={process.env.PUBLIC_URL + cats.image}
+              src={process.env.PUBLIC_URL + catArray.image}
               alt="Cats Profiles"
             />
             <br />
             <div class="card-text">
-              <h2>{cats.name}</h2>
-              <h3>{cats.description}</h3>
-              <h3>{cats.breed}</h3>
-              <h3>{cats.age}</h3>
-              <a href={cats.link} target="_blank" alt="Link to cats page">
+              <h2>{catArray.name}</h2>
+              <h3>{catArray.description}</h3>
+              <h3>{catArray.breed}</h3>
+              <h3>{catArray.age}</h3>
+              <a href={catArray.link} target="_blank" alt="Link to cats page">
                 {" "}
                 Link
               </a>
